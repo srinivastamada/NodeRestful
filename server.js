fef7c6b3f63789c58b7712f22f9b49edbfae58d7
Srinivas Tamada <srinivas_tamada@homedepot.com>
2017-02-03 11:32:59 -0500

    Post Logic

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('hapi');
 const mysql = require('mysql');
+const Joi = require('joi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 
@@ -28,32 +29,83 @@ server.route({
     path: '/users',
     handler: function (request, reply) {
 
-        connection.query('SELECT * FROM users', function (error, results, fields) {
+        connection.query('SELECT uid, username FROM users', function (error, results, fields) {
             if (error) throw error;
             console.log(results);
             reply(results);
         });
-        connection.end();
+
+
         // return reply('hello world');
     }
 });
 
 server.route({
-    method: 'POST',
-    path: '/usernames',
+    method: 'GET',
+    path: '/user/{uid}',
     handler: function (request, reply) {
+        const uid = request.params.uid;
 
-        connection.query('SELECT username FROM users', function (error, results, fields) {
+        connection.query('SELECT uid, username FROM users WHERE uid = "' + uid + '"', function (error, results, fields) {
             if (error) throw error;
             console.log(results);
             reply(results);
         });
-        connection.end();
-        // return reply('hello world');
+
+    },
+    config: {
+        validate: {
+            params: {
+                uid: Joi.number().integer()
+            }
+        }
     }
 });
 
 
+
+server.route({
+    method: 'POST',
+    path: '/messages',
+    config: {
+        handler: function (request, reply) {
+
+            const uid = request.payload.uid;
+            console.log(uid);
+
+            connection.query('SELECT * FROM messages WHERE uid_fk = "' + uid + '"', function (error, results, fields) {
+                if (error) throw error;
+                console.log(results);
+                reply(results);
+            });
+
+        },
+
+        validate: {
+            payload: {
+                uid: Joi.number().integer()
+            }
+        }
+
+    }
+
+});
+
+server.route({
+    method: 'DELETE',
+    path: '/user/{id}',
+    handler: function (request, reply) {
+        // if (quotes.length <= request.params.id) {
+        //   return reply('No quote found.').code(404);
+        // }
+        // quotes.splice(req.params.id, 1);
+        // reply(true);
+    }
+});
+
+
+
+//connection.end();
 
 // Start the server
 server.start((err) => {
